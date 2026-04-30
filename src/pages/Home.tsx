7c6e0aa34f2d16c01c7addbc843542823767e60e
skyhbui-hub <skyhbui@gmail.com>
2026-04-29 18:30:46 -0700

    Edited website description & CSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,10 +16,9 @@ function Home() {
           Description
         </h2>
         <p className='pb-6'>
-          Our grocery price comparison tool makes it simple to find the best deals near you. Powered by the Kroger API, our platform pulls real-time pricing data
-          from Kroger's family of banner stores in your area, giving you a side-by-side comparison of prices across multiple locations. Filter results by store, price,
-          or distance to find the option that works best for you. Create a free account to save and revisit your shopping lists anytime, or use the site as a guest with
-          a temporary session list.
+          Our grocery & recipe search tool makes it simple to decide the best ingredients to feed you and your loved ones. Powered by the fatSecret Platform API and MealsDB, our platform 
+          allows you to search for the best ingredients, their nutritional value, and where to purchase them. Additionally, this platform carries ease of mind when looking for recipes.
+          Specifically, it offers various recipes from around the world, recipe instructions, and relevantly, the ingredients to make it. Worry no more. This week's meal is powered by GrocerEase.
         </p>
       </section>
 
@@ -28,8 +27,8 @@ function Home() {
           Purpose
         </h2>
         <p className='pb-6'>
-          We've all been there, you buy groceries one day only to find the same items cheaper at a different store a few days later. That frustration is exactly what we set
-          out to fix. Our goal is to take the guesswork out of grocery shopping by putting price comparisons from multiple stores right at your fingertips. Whether you're
+          We've all been there, you can't figure out what to make that is best for you and your family. The problem is that it becomes easy to choose fast food over a homecooked meal when you don't know what to make.
+          That frustration is exactly what we set out to fix. Our goal is to take the guesswork out of grocery shopping by putting nutritional from multiple stores right at your fingertips. Whether you're
           trying to stretch your budget, save time, or just avoid buyer's remorse at the checkout line, our platform helps you make smarter, more informed decisions every time you shop.
         </p>
       </section>
